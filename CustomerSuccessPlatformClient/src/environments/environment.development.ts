cea8b6d85f440be52f342d65ee6c241d88398ce7
sunny2883 <sunnybhushan9185@gmail.com>
2024-04-02 13:01:53 +0530

    Adds All necessary files

---
+++ CustomerSuccessPlatformClient/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    apiUrl : "http://13.232.144.197:9500",
+    clientURL : "https://13.232.144.197:9501/",
+    AUTH0_DOMAIN:'dev-ekehvbfll57tols1.us.auth0.com',
+    AUTH0_CLIENT_ID:'BGdkQ7i8w9l9ZMAxAWe8EZg13kPmvE0O',
+};
---
+++ CustomerSuccessPlatformClient/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    apiUrl : "http://13.232.144.197:9500",
+    clientURL : "https://13.232.144.197:9501",
+    AUTH0_DOMAIN:'dev-w2cpuspyl8y04bzh.us.auth0.com',
+    AUTH0_CLIENT_ID:'l6AoZ9MlugUZmmFY83P58BA2YQargRJ3',
+};
